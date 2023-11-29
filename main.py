a836f6b0bc0f2bcb04a143f7ebfb0c4f73a80ada
Leo Chen <leo.youyaochen@gmail.com>
2023-11-29 11:28:14 +0800

    finish 4 classifiers, train function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import config as cfg
-import function  as fn
+import utils
 import model.linear_classifier as mLC
 import model.knn_classifier as mKNN
 import model.naive_decision_tree_classifer as mDT
@@ -11,10 +11,10 @@ import model.pruned_decision_tree_classifer as mPDT
 STAGE1: Setup data and classifier
 '''
 # Generate the data.
-X_train, X_test, Y_train, Y_test = fn.gen_train_test_data(cfg.datapath, cfg.train_test_split_ratio)
+X_train, X_test, Y_train, Y_test = utils.gen_train_test_data(cfg.datapath, cfg.train_test_split_ratio)
 # Preprocess the data.
-X_train_normalized = fn.do_minMaxNormalization(X_train)
-X_test_normalized = fn.do_minMaxNormalization(X_test)
+X_train_normalized = utils.do_minMaxNormalization(X_train)
+X_test_normalized = utils.do_minMaxNormalization(X_test)
 # Declare a classifier. 
 clf = mLC.LinearClassifier(num_features = X_train.shape[1])
 ### clf = mKNN.KNNClassifier()
@@ -32,11 +32,11 @@ clf.fit(X_train_normalized, Y_train)
 
 
 # Implement permutation importance algorithm (PIA)
-feature_importance_list = fn.cal_pertubation_importance(clf, X_test_normalized, Y_test, cfg.clf_name)
+feature_importance_list = utils.cal_pertubation_importance(clf, X_test_normalized, Y_test, cfg.clf_name)
 feature_dict = {"feature_importance_list":feature_importance_list}
 filename = './data/feature_importance/'+  f'{cfg.clf_name}_PermutImport_{clf.time}'
-fn.save_dict(filename + '.csv', feature_dict)
-fn.save_bar_chart(filename +'.png', cfg.feature_label_list, feature_importance_list, 'importance')
+utils.save_dict(filename + '.csv', feature_dict)
+utils.save_bar_chart(filename +'.png', cfg.feature_label_list, feature_importance_list, 'importance')
 
 
 # Implement SHAP
